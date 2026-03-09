1080275f6869d579c641e831302a72551d068ee1
lime0440 <limevladz@gmail.com>
2026-03-09 19:51:08 +0800

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import EmilFitnessMobileDashboard from "./emil_fitness_mobile_dashboard.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <EmilFitnessMobileDashboard />
+  </React.StrictMode>
+);
